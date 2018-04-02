544e486061f9d3f0779149842897a59a1d8aff18
daniielperez <daniel.perezluna94@gmail.com>
2018-04-02 18:19:55 -0500

    dist

--- dist/out-tsc/src/app/login/login.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var index_1 = require("./index");
-exports.LoginRoutes = [
-    {
-        path: '', component: index_1.LoginComponent
-    }
-];
-//# sourceMappingURL=login.routes.js.map
