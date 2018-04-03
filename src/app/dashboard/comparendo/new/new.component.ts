e838f01631b3cb95fe88fc37e1613d34ef7463a5
davidperezluna <david.perez.udenar@gmail.com>
2018-04-03 08:39:20 -0500

    comit

--- src/app/dashboard/comparendo/new/new.component.ts
+++ src/app/dashboard/comparendo/new/new.component.ts
@@ -4,6 +4,10 @@ import {ComparendoService} from '../../../services/comparendo.service';
 import {LoginService} from '../../../services/login.service';
 import {AgenteTransitoService} from '../../../services/agenteTransito.service';
 import {SedeOperativaService} from '../../../services/sedeOperativa.service';
+import {MunicipioService} from '../../../services/municipio.service';
+import {VehiculoService} from '../../../services/vehiculo.service';
+import {CiudadanoService} from '../../../services/ciudadano.service';
+import {CiudadanoVehiculoService} from '../../../services/ciudadanoVehiculo.service';
 import swal from 'sweetalert2';
 
 @Component({
@@ -17,7 +21,9 @@ public errorMessage;
 public respuesta;
 public agentesTransito:any;
 public sedesOperativas:any;
+public municipios:any;
 public agenteTransitoSelected:any;
+public municipioSelected:any;
 public sedeOperativaSelected:any;
 public agenteTransito:any;
 public sedeOperativa:any;
@@ -25,20 +31,50 @@ public agenteTransitoReady = false;
 public sedeOperativaReady = false;
 public validado = false;
 public comparendoExistente = false;
+public placa:any;
+public identificacion:any;
+public vehiculo:any;
+public ciudadano:any;
+public ciudadanosVehiculo:any;
+public vehiculoNoEncontrado=false;
+public ciudadanoNoEncontrado=false;
 
 constructor(
   private _ComparendoService: ComparendoService,
   private _loginService: LoginService,
   private _agenteTransitoService: AgenteTransitoService,
   private _sedeOperativaService: SedeOperativaService,
+  private _municipioService: MunicipioService,
+  private _vechiculoService: VehiculoService,
+  private _ciudadanoService: CiudadanoService,
+  private _ciudadanoVehiculoService: CiudadanoVehiculoService,
   ){}
 
   ngOnInit() {
+   this.placa = {
+     'placa' : this.placa,
+   }; 
+   this.identificacion = {
+     'numeroIdentificacion' : this.identificacion,
+   }; 
     this.comparendo = new Comparendo(null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null);
 
     this._agenteTransitoService.getAgenteTransitoSelect().subscribe(
         response => {
           this.agentesTransito = response;
+        }, 
+        error => {
+          this.errorMessage = <any>error;
+
+          if(this.errorMessage != null){
+            console.log(this.errorMessage);
+            alert("Error en la petición");
+          }
+        }
+      );
+    this._municipioService.getMunicipioSelect().subscribe(
+        response => {
+          this.municipios = response;
         }, 
         error => {
           this.errorMessage = <any>error;
@@ -170,6 +206,65 @@ constructor(
 
     }); 
     
+  }
+
+
+  onKeyPlaca(){
+  this.vehiculoNoEncontrado = false;
+  let token = this._loginService.getToken();
+  this._vechiculoService.showVehiculoPlaca(token,this.placa).subscribe(
+    response => {
+        if (response.status == "success") {
+        this.vehiculo = response.data;
+        this._ciudadanoVehiculoService.showCiudadanoVehiculoId(token,this.vehiculo.id).subscribe(
+            response => {
+              this.ciudadanosVehiculo = response.data;
+            error => {
+                this.errorMessage = <any>error;
+                if(this.errorMessage != null){
+                  console.log(this.errorMessage);
+                  alert("Error en la petición"); 
+                }
+              }
+          });
+        }else{
+        this.vehiculoNoEncontrado = true;
+        this.vehiculo = false;
+        }
+        console.log(this.vehiculo);
+      error => {
+          this.errorMessage = <any>error;
+          if(this.errorMessage != null){
+            console.log(this.errorMessage);
+            alert("Error en la petición"); 
+          }
+        }
+
+    }); 
+    
+  }
+
+  onKeyIdentificacion(){
+    this.ciudadanoNoEncontrado = false;
+    let token = this._loginService.getToken();
+    this._ciudadanoService.showCiudadanoCedula(token,this.identificacion).subscribe(
+      response => {
+        if (response.status == "success") {
+        this.ciudadano = response.data;
+        }else{
+        this.ciudadanoNoEncontrado = true;
+        this.ciudadano = false;
+        }
+        console.log(this.vehiculo);
+      error => {
+          this.errorMessage = <any>error;
+          if(this.errorMessage != null){
+            console.log(this.errorMessage);
+            alert("Error en la petición"); 
+          }
+        }
+
+    }); 
   }
 
 }
