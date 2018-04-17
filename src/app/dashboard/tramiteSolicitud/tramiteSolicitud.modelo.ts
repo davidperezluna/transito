d388e973f566eb347af6a821ce9af0f2ed8a6950
daniielperez <daniel.perezluna94@gmail.com>
2018-04-17 17:38:58 -0500

    sync

--- src/app/dashboard/tramiteSolicitud/tramiteSolicitud.modelo.ts
+++ src/app/dashboard/tramiteSolicitud/tramiteSolicitud.modelo.ts
@@ -2,6 +2,7 @@ export class TramiteSolicitud{
 	constructor(
 		public id: number,
 		public tramiteFacturaId: number,
+		public solicitanteId: number,
 		public observacion: string,
 		public documentacion: boolean,
 		public datos: string
