4eccc9171a61389e92cff9875a5a26b8887d4e0f
davidperezluna <david.perez.udenar@gmail.com>
2018-04-17 14:09:18 -0500

    comit

--- src/app/dashboard/gestionTransportePublico/gestionTransportePublico.modelo.ts
+++
@@ -1,7 +0,0 @@
-export class gestionTransportePublico{
-	constructor(
-		public estado:number,
-		public claseId:number,
-		public sedeOperativaId:number,
-	){}
-}
---
+++ src/app/dashboard/marca - copia/marca.routes.ts
@@ -0,0 +1,10 @@
+import { Route } from '@angular/router';
+import { MarcaComponent } from './index';
+
+export const MarcaRoutes: Route[] = [
+    {
+      path: 'marca',
+      component: MarcaComponent
+    }
+];
+ 
