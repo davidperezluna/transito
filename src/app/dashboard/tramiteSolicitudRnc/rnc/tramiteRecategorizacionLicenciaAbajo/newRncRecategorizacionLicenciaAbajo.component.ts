f1d3f5fab1c91629ed1f0a126cec65e2e0e1fc86
Edgar Guerrero <edgargn87@gmail.com>
2018-07-10 09:38:43 -0500

    Actualizacion RNC
    Vistas tramites RNC

---
+++ src/app/dashboard/tramiteSolicitudRnc/rnc/tramiteRecategorizacionLicenciaAbajo/newRncRecategorizacionLicenciaAbajo.component.ts
@@ -0,0 +1,126 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { TramiteFacturaService } from '../../../../services/tramiteFactura.service';
+import { ClaseService } from '../../../../services/clase.service';
+import { ServicioService } from '../../../../services/servicio.service';
+import { CiudadanoService } from '../../../../services/ciudadano.service';
+import { PaisService } from '../../../../services/pais.service';
+import { LoginService } from '../../../../services/login.service';
+
+import swal from 'sweetalert2';
+
+@Component({
+    selector: 'appRnc-recategorizacion-licencia-abajo',
+    templateUrl: './newRncRecategorizacionLicenciaAbajo.html'
+})
+export class NewRncRecategorizacionLicenciaAbajoComponent implements OnInit {
+    @Output() readyTramite = new EventEmitter<any>();
+    @Output() cancelarTramite = new EventEmitter<any>();
+    @Input() solicitante: any = null;
+    @Input() factura: any = null;
+    public errorMessage;
+    public respuesta;
+    public clases: any;
+    public claseSelected: any;
+    public servicios: any;
+    public servicioSelected: any;
+    public paises: any;
+    public paisSelected: any;
+    public tramiteFacturaSelected: any;
+    public tipoCambioSelected: any;
+    public categorias: string[];
+    public datos = {
+        'tramiteFactura': null,
+        'categoria': null,
+        'licenciaConduccion': null,
+        'numeroRunt': null,
+        'vigencia': null,
+        'paisId': null,
+        'claseId': null,
+        'servicioId': null,
+        'solicitanteId': null,
+    };
+
+    constructor(
+        private _LoginService: LoginService,
+        private _tramiteFacturaService: TramiteFacturaService,
+        private _ClaseService: ClaseService,
+        private _ServicioService: ServicioService,
+        private _CiudadanoService: CiudadanoService,
+        private _PaisService: PaisService,
+    ) { }
+
+    ngOnInit() {
+        this.categorias = ['A2'];
+
+        this._ClaseService.getClaseSelect().subscribe(
+            response => {
+              this.clases = response;
+            },
+            error => {
+              this.errorMessage = <any>error;
+      
+              if(this.errorMessage != null){
+                console.log(this.errorMessage);
+                alert('Error en la petición');
+              }
+            }
+        );
+
+        this._ServicioService.getServicioSelect().subscribe(
+            response => {
+              this.servicios = response;
+            },
+            error => {
+              this.errorMessage = <any>error;
+      
+              if(this.errorMessage != null){
+                console.log(this.errorMessage);
+                alert('Error en la petición');
+              }
+            }
+        );
+
+        this._PaisService.select().subscribe(
+            response => {
+              this.paises = response;
+            },
+            error => {
+              this.errorMessage = <any>error;
+      
+              if(this.errorMessage != null){
+                console.log(this.errorMessage);
+                alert('Error en la petición');
+              }
+            }
+        );
+    }
+    
+    enviarTramite() {
+        let token = this._LoginService.getToken();
+        
+        this.datos.tramiteFactura = 58;
+        this.datos.claseId = this.claseSelected;
+        this.datos.servicioId = this.servicioSelected;
+        this.datos.paisId = this.paisSelected;
+        this.datos.solicitanteId = this.solicitante.id;
+
+        this._CiudadanoService.editLicenciaConduccion(this.datos,token).subscribe(
+            response => {
+                error => {
+                    this.errorMessage = <any>error;
+
+                    if(this.errorMessage != null){
+                        console.log(this.errorMessage);
+                        alert("Error en la petición");
+                    }
+                }
+            }
+        );
+
+        this.readyTramite.emit(this.datos);
+    }
+    onCancelar(){
+        this.cancelarTramite.emit(true);
+    }
+
+}
