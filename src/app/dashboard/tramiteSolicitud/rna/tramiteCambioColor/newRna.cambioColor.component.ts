b463864d88cc392b2e752428526408e7144f64e3
andrescalpa <andrescalpa7@gmail.com>
2018-07-12 18:00:14 -0500

    sync

--- src/app/dashboard/tramiteSolicitud/rna/tramiteCambioColor/newRna.cambioColor.component.ts
+++ src/app/dashboard/tramiteSolicitud/rna/tramiteCambioColor/newRna.cambioColor.component.ts
@@ -49,11 +49,7 @@ export class NewRnaCambioColorComponent implements OnInit {
         });
         //consultar tramite solicitud con tramiterealizado.id
         let token = this._loginService.getToken();
-<<<<<<< HEAD
-        if(this.tramiteRealizado){
-=======
         if (this.tramiteRealizado) {
->>>>>>> 59dd64a1c8f5f43b842150b5bcda724a0f29b2c1
             this._TramiteSolicitudService.showTramiteSolicitudByTamiteFactura(token,this.tramiteRealizado.id).subscribe(
                 response => {
                     this.datos = response.data.datos
