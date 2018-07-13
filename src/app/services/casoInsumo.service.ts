8862e219adb2535b2441b7da7a20a1c8b4d84d68
daniielperez <daniel.perezluna94@gmail.com>
2018-07-13 08:35:33 -0500

    sync

---
+++ src/app/services/casoInsumo.service.ts
@@ -0,0 +1,60 @@
+import  {Injectable} from "@angular/core";
+import  {Http, Response,Headers} from "@angular/http";
+import  "rxjs/add/operator/map";
+import { environment } from 'environments/environment';
+
+@Injectable()
+export class CasoInsumoService {
+	private url = environment.apiUrl + "casoinsumo";
+	public identity;
+	public token;
+
+	constructor(private _http: Http){}
+
+	getCasoInsumo(){ 
+		
+		return this._http.get(this.url+"/").map(res => res.json());
+	}
+
+	register(color,token){
+		
+		let json = JSON.stringify(color);
+		let params = "json="+json+"&authorization="+token;
+		let headers = new Headers({'Content-Type':'application/x-www-form-urlencoded'});
+		return this._http.post(this.url+"/new", params, {headers: headers})
+							  .map(res => res.json());
+	}
+
+	deleteCasoInsumo(token,id){
+
+		let params = "authorization="+token;
+		let headers = new Headers({'Content-Type':'application/x-www-form-urlencoded'});
+		return this._http.post(this.url+"/"+id+"/delete", params, {headers: headers})
+							  .map(res => res.json());
+	}
+
+	showCasoInsumo(token,id){
+		
+		let params = "authorization="+token;
+		let headers = new Headers({'Content-Type':'application/x-www-form-urlencoded'});
+		return this._http.post(this.url+"/show/"+id, params, {headers: headers})
+							  .map(res => res.json());
+
+	}
+
+	editCasoInsumo(color,token){
+
+		let json = JSON.stringify(color);
+		let params = "json="+json+"&authorization="+token;
+		let headers = new Headers({'Content-Type':'application/x-www-form-urlencoded'});
+ 			return this._http.post(this.url+"/edit", params, {headers: headers})
+							  .map(res => res.json());
+
+	}
+
+	getCasoInsumoSelect(){
+		
+		return this._http.get(this.url+"/select").map(res => res.json());
+	}
+	
+}
