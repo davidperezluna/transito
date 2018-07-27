5646978980fab87fc3e2dbfe635c39dbbcf987e3
Marcos Rosero <marcostever77@gmail.com>
2018-07-27 18:10:25 -0500

    sync

---
+++ src/app/dashboard/rnmaTramiteLevantamientoLimitacion/rnmaTramiteLevantamientoLimitacion.routes.ts
@@ -0,0 +1,9 @@
+import { Route } from '@angular/router';
+import { RnmaTramiteLevantamientoLimitacionComponent } from '.';
+
+export const TramiteLevantamientoLimitacionRoutes: Route[] = [
+    {
+      path: 'rnmaTramiteLevantamientoLimitacion',
+    component: RnmaTramiteLevantamientoLimitacionComponent
+    }
+];
