1d3fc75f1eac7abbfc15f56f054ce7d78afbe3ef
Marcos Rosero <marcostever77@gmail.com>
2018-07-27 12:02:34 -0500

    limitacion correccion varios vehiculos

--- src/app/dashboard/rnmaTramiteInscripcionLimitacion/new/new.component.ts
+++ src/app/dashboard/rnmaTramiteInscripcionLimitacion/new/new.component.ts
@@ -207,36 +207,7 @@ export class NewComponent implements OnInit {
             type: 'success',
             confirmButtonText: 'Aceptar'
           })
-          // this._VehiculoLimitacionService.register(this.datos2, token).subscribe(
-          //   response => {
-          //     this.respuesta = response;
-          //     console.log(this.respuesta);
-          //     if (this.respuesta.status == 'success') {
-          //       this.ready.emit(true);
-          //       swal({
-          //         title: 'Perfecto!',
-          //         text: 'Registro exitoso!',
-          //         type: 'success',
-          //         confirmButtonText: 'Aceptar'
-          //       })
-          //     } else {
-          //       swal({
-          //         title: 'Error!',
-          //         text: 'La limitacion ya se encuentra registrado',
-          //         type: 'error',
-          //         confirmButtonText: 'Aceptar'
-          //       })
-          //     }
-          //     error => {
-          //       this.errorMessage = <any>error;
-
-          //       if (this.errorMessage != null) {
-          //         console.log(this.errorMessage);
-          //         alert("Error en la petición");
-          //       }
-          //     }
-
-          //   });
+          
         } else {
           swal({
             title: 'Error!',
