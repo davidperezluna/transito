1b277156f615e33538fc8b699708156e9b0c2723
daniielperez <daniel.perezluna94@gmail.com>
2018-07-27 18:04:21 -0500

    Sync

--- src/app/services/cfgValorVehiculo.service.ts
+++ src/app/services/cfgValorVehiculo.service.ts
@@ -52,6 +52,14 @@ export class CfgValorVehiculoService {
 
 	}
 
+	getCfgValorVehiculoVehiculo(datos,token){
+		let json = JSON.stringify(datos);
+		let params = "json="+json+"&authorization="+token;
+		let headers = new Headers({'Content-Type':'application/x-www-form-urlencoded'});
+ 			return this._http.post(this.url+"/show/vehiculo/", params, {headers: headers})
+							  .map(res => res.json());
+	}
+
 	getCfgValorVehiculoSelect(){
 		
 		return this._http.get(this.url+"/select").map(res => res.json());
