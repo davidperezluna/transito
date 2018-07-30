5378aada64c997950fc98b0d296dcad95d0936d1
ivansolarte86 <ivansolarte86@gmail.com>
2018-07-30 08:58:31 -0500

    sync

--- src/app/dashboard/reporte/multa/multa.component.ts
+++ src/app/dashboard/reporte/multa/multa.component.ts
@@ -38,147 +38,17 @@ constructor(
 ){}
 
 ngOnInit() {
-  // this.registroMaquinaria = new Reporte();
-  
   
   }
 
   onCancelar(){
       this.ready.emit(true);
   }
-  onEnviar(){
-    // let token = this._loginService.getToken();
-    // this.registroMaquinaria.tipoVehiculoId = this.combustibleSelected;
-    // this.registroMaquinaria.cfgOrigenVehiculoId = this.cfgOrigenRegistroSelected;
-    
-    // this.registroMaquinaria.vehiculoColorId = this.colorSelected;
-    // this.registroMaquinaria.vehiculoMarcaId = this.marcaSelected;
-    // this.registroMaquinaria.vehiculoClaseId = this.claseSelected;
-    // this.registroMaquinaria.vehiculoLineaId = this.lineaSelected;
-    // this.registroMaquinaria.vehiculoCarroceriaId = this.carroceriaSelected;
-    // this.registroMaquinaria.vehiculoCombustibleId = this.combustibleSelected;
-
-    // this.registroMaquinaria.condicionSelected = this.condicionSelected;
-    // this.registroMaquinaria.fechaIngreso = this.fechaIngreso;
-    // this.registroMaquinaria.pesoBruto = this.pesoBruto;
-    // this.registroMaquinaria.cargaUtilMaxima = this.cargaUtilMaxima;
-    // this.registroMaquinaria.rodajeSelected = this.rodajeSelected;
-    // this.registroMaquinaria.numeroEjes = this.numeroEjes;
-    // this.registroMaquinaria.numeroLlantas = this.numeroLlantas;
-    // this.registroMaquinaria.tipoCabinaSelected = this.tipoCabinaSelected;
-    // this.registroMaquinaria.altoTotal = this.altoTotal;
-    // this.registroMaquinaria.largoTotal = this.largoTotal;
-    // this.registroMaquinaria.anchoTotal = this.anchoTotal;
-    // this.registroMaquinaria.subpartidaArancelaria = this.subpartidaArancelaria;
-    // console.log(this.registroMaquinaria);  
-    
-    // var html = 'los datos de la maquinaria a ingresar son:<br>'+
-    //            'Placa: <b>'+this.registroMaquinaria.vehiculoPlaca+'</b><br>'+
-    //            'Condicon ingreso: <b>'+this.registroMaquinaria.condicionSelected+'</b><br>'+
-    //            'Motor: <b>'+this.registroMaquinaria.vehiculoMotor+'</b><br>'+
-    //            'Serie: <b>'+this.registroMaquinaria.vehiculoSerie+'</b><br>'+
-    //            'Chasis: <b>'+this.registroMaquinaria.vehiculoChasis+'</b><br>'+
-    //            'Fecha ingreso: <b>'+this.registroMaquinaria.fechaIngreso+'</b><br>';
-               
-
-  //  swal({
-  //     title: 'Preregistro de maquinaria!',
-  //     type: 'warning',
-  //     html:html,
-  //     showCancelButton: true,
-  //     focusConfirm: false,
-  //     confirmButtonText:
-  //       '<i class="fa fa-thumbs-up"></i> Crear!',
-  //     confirmButtonAriaLabel: 'Thumbs up, great!',
-  //     cancelButtonText:
-  //     '<i class="fa fa-thumbs-down"></i> No crear',
-  //     cancelButtonAriaLabel: 'Thumbs down',
-  //   }).then((result) => {
-  //       if (result.value) {
-
-  //   this._RegistroMaquinariaService.register(this.registroMaquinaria,token).subscribe(
-	// 		response => {
-  //       this.respuesta = response;
-  //       console.log(this.respuesta);
-  //       if(this.respuesta.status == 'success'){
-  //         this.ready.emit(true);
-  //         swal({
-  //           title: 'Perfecto!',
-  //           text: 'Registro exitoso!',
-  //           type: 'success',
-  //           confirmButtonText: 'Aceptar'
-  //         })
-  //       }else{
-  //         swal({
-  //           title: 'Error!',
-  //           text: 'El vehiculo '+ this.registroMaquinaria.altoTotal +' ya se encuentra registrado',
-  //           type: 'error',
-  //           confirmButtonText: 'Aceptar'
-  //         })
-  //       }
-	// 		error => {
-	// 				this.errorMessage = <any>error;
-
-	// 				if(this.errorMessage != null){
-	// 					console.log(this.errorMessage);
-	// 					alert("Error en la petición");
-	// 				}
-	// 			}
-
-    // }); 
-    //     } else if (
-    //       // Read more about handling dismissals
-    //       result.dismiss === swal.DismissReason.cancel
-    //     ) {
-
-    //     }
-    //   })
+  generar(){
+    // generar la lista de las infracciones
   }
   changedMarca(e){
-    // if (this.marcaSelected) {
-    //   let token = this._loginService.getToken()
-    //     this._lineaService.getLineasMar(this.marcaSelected, token).subscribe(
-    //       response => { 
-    //         if (response.data[0] != null) {
-    //           this.lineas = response.data;
-    //         }else{
-    //           this.lineas = [];
-    //         }
-    //       }, 
-    //       error => { 
-    //         this.errorMessage = <any>error;
     
-    //         if(this.errorMessage != null){
-    //           console.log(this.errorMessage);
-    //           alert("Error en la petición");
-    //         }
-    //       }
-    //     );
-    // }
     }
 
-
-  changedDepartamento(e){
-    // if (this.marcaSelected) {
-    //   let token = this._loginService.getToken()
-    //     this._lineaService.getLineasMar(this.marcaSelected, token).subscribe(
-    //       response => {
-    //         console.log(response.data[0]);
-    //         if (response.data[0] != null) {
-    //           this.lineas = response.data;
-    //         }else{
-    //           this.lineas = [];
-    //         }
-    //       }, 
-    //       error => { 
-    //         this.errorMessage = <any>error;
-    
-    //         if(this.errorMessage != null){
-    //           console.log(this.errorMessage);
-    //           alert("Error en la petición");
-    //         }
-    //       }
-    //     );
-    // }
-    }
 }
