851b74e468aba9a4de9234587c314e31efc77d81
andrescalpa <andrescalpa7@gmail.com>
2018-08-11 12:14:29 -0500

    Sync

--- src/app/dashboard/msvEvaluacion/newFortalecimiento/newFortalecimiento.component.ts
+++ src/app/dashboard/msvEvaluacion/newFortalecimiento/newFortalecimiento.component.ts
@@ -13,7 +13,7 @@ declare var $: any;
 })
 export class NewFortalecimientoComponent implements OnInit {
   @Input() msvCategoriaId;
-  @Input() empresa;
+  @Input() miEmpresa;
   public msj = '';
   public showT = false;
   public msvParametros;
@@ -23,7 +23,7 @@ export class NewFortalecimientoComponent implements OnInit {
   public criterio;
   public aplica;
   public evidencia;
-  public datos: any = [];
+  public datos: any = []; 
 
   constructor(
     private _loginService: LoginService,
@@ -35,15 +35,12 @@ export class NewFortalecimientoComponent implements OnInit {
 
   ngOnInit() {
     
-    let token = this._loginService.getToken();
-    console.log(this.empresa);
+    let token = this._loginService.getToken();    
     this._MsvParametroService.getParametroByCategoriaId(token,this.msvCategoriaId).subscribe(
       response => {
         this.msvParametros = response.data;
         if (this.msvParametros) {
-          //entra aquí si encuentra Parametro   
-          console.log(this.msvParametros);
-                 
+          //entra aquí si encuentra Parametro                    
           this.showT = true;
       } else {
           swal({
@@ -60,7 +57,7 @@ export class NewFortalecimientoComponent implements OnInit {
   onEnviar() {
     console.log(this.msvParametros);
     let token = this._loginService.getToken();
-    this._MsvCalificacionService.editCalificacion(token,this.msvParametros).subscribe();
+    this._MsvCalificacionService.newCalificacion(token,this.msvParametros,this.miEmpresa.id).subscribe();
     
   }
 }
