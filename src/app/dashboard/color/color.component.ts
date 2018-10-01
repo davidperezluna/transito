2c52bee6f562de9ac1617f53992056a4546d03f3
Edgar Guerrero <edgargn87@gmail.com>
2018-10-01 10:41:49 -0500

    Sync

--- src/app/dashboard/color/color.component.ts
+++ src/app/dashboard/color/color.component.ts
@@ -100,7 +100,7 @@ export class ColorComponent implements OnInit {
     }).then((result) => {
       if (result.value) {
         let token = this._loginService.getToken();
-        this._ColorService.deleteColor(token,id).subscribe(
+        this._ColorService.delete(token,id).subscribe(
             response => {
                 swal({
                       title: 'Eliminado!',
