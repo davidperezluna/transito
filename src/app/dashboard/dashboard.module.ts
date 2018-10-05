ee5b541be55956ebb8408467e9cd45f70a9a5c61
Edgar Guerrero <edgargn87@gmail.com>
2018-10-05 17:54:36 -0500

    Sync

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -27,6 +27,8 @@ import { RpcccInventarioDocumentalModule } from './rpcccInventarioDocumental/rpc
 
 import { CfgAdmFormatoTipoModule } from './cfgAdmFormatoTipo/cfgAdmFormatoTipo.module';
 
+import { CfgEmpresaServicioModule } from './cfgEmpresaServicio/cfgEmpresaServicio.module';
+
 import { CfgBodegaModule } from './cfgBodega/cfgBodega.module';
 import { CfgSvConectorModule } from './cfgSvConector/cfgSvConector.module';
 import { CfgSvSenialTipoModule } from './cfgSvSenialTipo/cfgSvSenialTipo.module';
@@ -298,7 +300,7 @@ import { MsvSenialInventarioModule } from './msvSenialInventario/msvSenialInvent
       InsumoBusquedaModule,
       FacturaInfraccionModule,
       //MsvTCAsignacionModule,
-      //CfgEmpresaServicioModule,
+      CfgEmpresaServicioModule,
       GdCfgTipoCorrespondenciaModule,
       MsvSenialInventarioModule,
       RnaCertificadoTradicionOficialModule,
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl : 'http://190.146.7.242/colossus-sit/web/app.php/',
-  docsUrl : 'http://190.146.7.242/colossus-sit/web/docs/',
-  uploadUrl : 'http://190.146.7.242/colossus-sit/web/uploads/',
+  apiUrl: 'http://190.146.7.242:81/colossus-sit/web/app.php/',
+  docsUrl: 'http://190.146.7.242:81/colossus-sit/web/docs/',
+  uploadUrl: 'http://190.146.7.242:81/colossus-sit/web/uploads/',
 };
