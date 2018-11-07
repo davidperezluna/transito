c7cdd3c937ae41fd9ddf4b37399f4fd4109d4d9e
daniielperez <daniel.perezluna94@gmail.com>
2018-11-07 16:37:59 -0500

    Sync

--- src/app/dashboard/tramiteSolicitudRnrs/rnrs/tramiteTraspaso/newRnrs.traspaso.component.ts
+++ src/app/dashboard/tramiteSolicitudRnrs/rnrs/tramiteTraspaso/newRnrs.traspaso.component.ts
@@ -1,10 +1,6 @@
 import { Component , OnInit, Input, AfterViewInit, Output, EventEmitter } from '@angular/core';
-import { TramiteSolicitud } from '../../tramiteSolicitudRnrs.modelo';
-import { TramiteSolicitudService } from '../../../../services/tramiteSolicitud.service';
 import { CiudadanoVehiculoService } from '../../../../services/ciudadanoVehiculo.service';
-import { TramiteFacturaService } from '../../../../services/tramiteFactura.service';
 import { LoginService } from '../../../../services/login.service';
-import { ColorService } from '../../../../services/color.service';
 import { VehiculoService } from '../../../../services/vehiculo.service';
 import { CiudadanoService } from '../../../../services/ciudadano.service';
 import { Router } from "@angular/router";
