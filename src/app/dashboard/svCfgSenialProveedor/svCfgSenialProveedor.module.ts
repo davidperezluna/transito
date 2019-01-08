250e046d529834d6c6ec6c1707d4c0fa13a515fd
Edgar Guerrero <edgargn87@gmail.com>
2019-01-08 03:58:45 -0500

    Creacion CRUD SvSenialProveedor

---
+++ src/app/dashboard/svCfgSenialProveedor/svCfgSenialProveedor.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SvCfgSenialEstadoComponent } from './svCfgSenialEstado.component';
+import { Ng2BootstrapModule } from 'ng2-bootstrap';
+import { SvCfgSenialEstadoService } from '../../services/svCfgSenialEstado.service';
+
+import { NewComponent } from './new/new.component';
+import { EditComponent } from './edit/edit.component';
+import { SelectModule } from 'angular2-select';
+
+@NgModule({
+    imports: [CommonModule, Ng2BootstrapModule.forRoot(),SelectModule],
+    declarations: [SvCfgSenialEstadoComponent,NewComponent,EditComponent],
+    exports: [SvCfgSenialEstadoComponent, NewComponent,EditComponent],
+    providers:[SvCfgSenialEstadoService]
+})
+
+export class SvCfgSenialEstadoModule { }
