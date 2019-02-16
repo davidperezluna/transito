d95056f4d25ff30f4ad0f0788122b54d569620e6
JhoanCarlosama <jhoan.carlosama@gmail.com>
2019-02-16 12:06:28 -0500

    Sync

--- src/app/dashboard/msvEvaluacion/msvEvaluacion.module.ts
+++ src/app/dashboard/msvEvaluacion/msvEvaluacion.module.ts
@@ -10,7 +10,6 @@ import { MsvCalificacionService } from '../../services/msvCalificacion.service';
 import { MsvResultadoService } from "../../services/msvResultado.service";
 import { NewComponent } from './new/new.component';
 import { EditComponent } from './edit/edit.component';
-import { NewFortalecimientoComponent } from './newFortalecimiento/newFortalecimiento.component';
 import { SelectModule } from 'angular2-select';
 import { NewEmpresaComponent } from './newEmpresa/newEmpresa.component';
 import { NewRevisionComponent } from './newRevision/newRevision.component';
@@ -18,8 +17,8 @@ import { NewRevisionComponent } from './newRevision/newRevision.component';
 
 @NgModule({
     imports: [CommonModule, Ng2BootstrapModule.forRoot(),SelectModule],
-    declarations: [MsvEvaluacionComponent,NewComponent,EditComponent,NewEmpresaComponent,NewRevisionComponent,NewFortalecimientoComponent],
-    exports: [MsvEvaluacionComponent, NewComponent,EditComponent,NewEmpresaComponent,NewRevisionComponent, NewFortalecimientoComponent],
+    declarations: [MsvEvaluacionComponent,NewComponent,EditComponent,NewEmpresaComponent,NewRevisionComponent],
+    exports: [MsvEvaluacionComponent, NewComponent,EditComponent,NewEmpresaComponent,NewRevisionComponent],
     providers: [MsvRevisionService, MsvEvaluacionService, MsvParametroService, MsvVariableService, MsvCalificacionService, MsvResultadoService]
 })
 
--- src/app/dashboard/msvRevision/index.ts
+++
@@ -1,5 +0,0 @@
-/**
-*  This barrel file provides the export for the lazy loaded HomeComponent.
-*/
-export * from './msvRevision.component';
-export * from './msvRevision.routes';
