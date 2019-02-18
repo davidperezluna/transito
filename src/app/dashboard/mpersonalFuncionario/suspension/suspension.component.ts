e349936d46172075a0fd9ec2d464bcedb40e71c1
Edgar Guerrero <edgargn87@gmail.com>
2019-02-18 11:50:05 -0500

    Sync

--- src/app/dashboard/mpersonalFuncionario/suspension/suspension.component.ts
+++ src/app/dashboard/mpersonalFuncionario/suspension/suspension.component.ts
@@ -87,10 +87,10 @@ export class SuspensionComponent implements OnInit {
       sPaginationType: 'full_numbers',
       oLanguage: {
         oPaginate: {
-          sFirst: '<i class="fa fa-step-forward"></i>',
+          sFirst: '<i class="fa fa-step-backward"></i>',
           sPrevious: '<i class="fa fa-chevron-left"></i>',
           sNext: '<i class="fa fa-chevron-right"></i>',
-          sLast: '<i class="fa fa-step-backward"></i>'
+          sLast: '<i class="fa fa-step-forward"></i>'
         }
       }
    });
--- src/app/dashboard/rnaAsignacionInsumos/rnaAsignacionInsumos.component.ts
+++ src/app/dashboard/rnaAsignacionInsumos/rnaAsignacionInsumos.component.ts
@@ -108,10 +108,10 @@ export class rnaAsignacionInsumosComponent implements OnInit {
       ],
       oLanguage: {
         oPaginate: {
-          sFirst: '<i class="fa fa-step-forward"></i>',
+          sFirst: '<i class="fa fa-step-backward"></i>',
           sPrevious: '<i class="fa fa-chevron-left"></i>',
           sNext: '<i class="fa fa-chevron-right"></i>',
-          sLast: '<i class="fa fa-step-backward"></i>'
+          sLast: '<i class="fa fa-step-forward"></i>'
         }
       }
    });
