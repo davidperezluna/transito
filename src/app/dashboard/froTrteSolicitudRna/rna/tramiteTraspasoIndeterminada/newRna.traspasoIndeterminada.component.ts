de57b19104711483c3d7039091c7bedf60ad7880
Edgar Guerrero <edgargn87@gmail.com>
2019-03-27 18:49:03 -0500

    Update Tramites RNA

--- src/app/dashboard/froTrteSolicitudRna/rna/tramiteTraspasoIndeterminada/newRna.traspasoIndeterminada.component.ts
+++ src/app/dashboard/froTrteSolicitudRna/rna/tramiteTraspasoIndeterminada/newRna.traspasoIndeterminada.component.ts
@@ -20,26 +20,18 @@ export class NewRnaTraspasoIndeterminadaComponent implements OnInit {
   @Output() readyTramite = new EventEmitter<any>();
   @Input() vehiculo: any = null; 
   @Input() tramiteFactura: any = null;
-  @Input() ciudadano: any = null;
-  public errorMessage; public autorizado: any = true;
+  @Input() propietario: any = null;
+  public errorMessage; 
+  
+  public autorizado: any = false;
+  public tramiteSolicitud: any = null;
 
-  public codigoOrganismo;
-  public tipoServicio;
-  public nombreApoderado;
-  public tipoDocApoderado;
-  public idTramiteSolicitud;
-  public numeroDocumento;
   public date:any;
-  public formNew = false;
-  public formEdit = false;
-  public formIndex = true;
-  public table:any;    
   public entidadesJudiciales:any;    
-  public entidadJudicialSelected:any;    
   public vehiculos: any = false;
   public propietarioVehiculo;
-  public tipoSelected;
-  public viewApoderado: boolean;
+
+  public table:any;    
 
   public acta = {
     'fecha': null,
@@ -55,14 +47,15 @@ export class NewRnaTraspasoIndeterminadaComponent implements OnInit {
 
   public datos = {
     'fecha': null,
-    'tipoDocumentoSelected': null,
-    'tipoServicio': null,
+    'fechaActa': null,
+    'numeroActa': null,
+    'tipoTraspaso': null,
     'tipoDocApoderado': null,
     'nombreApoderado': null,
     'numeroDocumento': null,
-    'personaTraslado': 'SIN REGISTRO',
     'idFuncionario': null,
     'idSolicitante': null,
+    'idEntidadJudicial': null,
     'idVehiculo': null,
     'idOrganismoTransito': null,
     'idTramiteFactura': null,
@@ -135,29 +128,13 @@ export class NewRnaTraspasoIndeterminadaComponent implements OnInit {
       }
     }); 
     
-    this.tipoServicio = this.datos.tipoServicio;
     this.date = this.datos.fecha;
-    this.datos.nombreApoderado = this.ciudadano.usuario.primerNombre+" "+this.ciudadano.usuario.segundoNombre+" "+this.ciudadano.usuario.primerApellido;
-    this.datos.tipoDocApoderado = this.ciudadano.usuario.tipoIdentificacion.nombre;
-    this.datos.numeroDocumento = this.ciudadano.usuario.identificacion;   
-
-    this.nombreApoderado = this.datos.nombreApoderado;
-    this.tipoDocApoderado = this.datos.tipoDocApoderado;
-    this.numeroDocumento = this.datos.numeroDocumento;
-    this.datos.idSolicitante= this.ciudadano.id;
+    this.datos.idSolicitante = this.propietario.id;
   }
   
-  onNew(){
-    this.formNew = true;
-    this.formIndex = false;
-    this.table.destroy();
-  }
 
   ready(isCreado:any){
     if(isCreado) {
-      this.formNew = false;
-      this.formEdit = false;
-      this.formIndex = true;
       this.ngOnInit();
     }
   }
@@ -172,20 +149,20 @@ export class NewRnaTraspasoIndeterminadaComponent implements OnInit {
 
     let resumen = "<b>No. factura: </b>" + this.tramiteFactura.factura.numero;
 
-    /*this.tramiteSolicitud.datos = { 'foraneas': this.datos, 'resumen': resumen };
+    this.tramiteSolicitud.datos = { 'foraneas': this.datos, 'resumen': resumen };
     this.tramiteSolicitud.idVehiculo = this.vehiculo.id;
-    this.tramiteSolicitud.idCiudadano = this.ciudadano.id;
+    this.tramiteSolicitud.idCiudadano = this.propietario.id;
 
     this._TramiteSolicitudService.register(this.tramiteSolicitud, token).subscribe(
       response => {
         if (response.status == 'success') {
-          this.idTramiteSolicitud = response.idTramiteSolicitud;
+          //this.idTramiteSolicitud = response.idTramiteSolicitud;
 
           this._PropietarioService.update(this.datos, token).subscribe(
             response => {
               if (response.status == 'success') {
-                this.acta.tramiteSolicitud = this.idTramiteSolicitud;
-                this.acta.entidadJudicial = this.entidadJudicialSelected;
+                //this.acta.tramiteSolicitud = this.idTramiteSolicitud;
+                //this.acta.entidadJudicial = this.entidadJudicialSelected;
 
                 this._VhloActaTraspasoService.register(this.acta, token).subscribe(
                   response => {
@@ -235,6 +212,5 @@ export class NewRnaTraspasoIndeterminadaComponent implements OnInit {
         }
       }
     );
-    */
   }
 }
