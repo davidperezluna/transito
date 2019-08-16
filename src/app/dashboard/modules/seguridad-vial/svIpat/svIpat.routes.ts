3824ace199de0ad846f89af681f05a049fb4318d
Edgar Guerrero <edgargn87@gmail.com>
2019-08-16 08:26:40 -0500

    Sync

--- src/app/dashboard/modules/seguridad-vial/svIpat/svIpat.routes.ts
+++ src/app/dashboard/modules/seguridad-vial/svIpat/svIpat.routes.ts
@@ -1,14 +1,14 @@
 import { Route } from '@angular/router';
 import { SvIpatComponent } from '.';
-import { ExportComponent } from '.';
+//import { ExportComponent } from '.';
 
 export const SvIpatRoutes: Route[] = [
     {
     path: 'svIpat',
     component: SvIpatComponent
     },
-    {
+    /*{
         path: 'svIpat/export',
         component: ExportComponent
-    }
+    }*/
 ];
