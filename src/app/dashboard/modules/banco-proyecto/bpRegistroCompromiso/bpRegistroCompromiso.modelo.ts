667c68f3f683f4c92d9d832064b11c30bbfae266
Edgar Guerrero <edgargn87@gmail.com>
2019-09-01 09:56:19 -0500

    Sync

--- src/app/dashboard/modules/banco-proyecto/bpRegistroCompromiso/bpRegistroCompromiso.modelo.ts
+++ src/app/dashboard/modules/banco-proyecto/bpRegistroCompromiso/bpRegistroCompromiso.modelo.ts
@@ -1,6 +1,7 @@
 export class BpRegistroCompromiso{
 	constructor(
 		public fechaExpedicion: string,
+		public numero: string,
 		public contratoNumero: string,
 		public contratoTipo: string,
 		public contratoEstado: string,
