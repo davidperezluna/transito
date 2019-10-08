d31d2f1aa1e13f6c148ec12ed20ac98c7456581c
Edgar Guerrero <edgargn87@gmail.com>
2019-10-08 08:15:47 -0500

    Sync

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,23 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { Ng2BootstrapModule } from 'ng2-bootstrap';
 import { ToolTipModule } from 'angular2-tooltip'
+import { Ng2BootstrapModule } from 'ng2-bootstrap';
 
-import { DashboardRoutes } from './dashboard.routes';
 import { DashboardComponent } from './dashboard.component'; 
+import { DashboardRoutes } from './dashboard.routes';
+
 import { TopNavComponent } from '../shared';
 import { SidebarComponent } from '../shared';
 import { FooterComponent } from '../shared';
 import { RightsidebarComponent } from '../shared';
+
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
     DashboardRoutes,
+    ToolTipModule,
     Ng2BootstrapModule.forRoot(),
-    ToolTipModule
+    HomeModule,
   ],
   declarations: [
     DashboardComponent, 
