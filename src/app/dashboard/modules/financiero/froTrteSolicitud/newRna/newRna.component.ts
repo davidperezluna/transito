cc84afc70c01abc4e551b8f896bb901e85b82fe1
JhoanCarlosama <jhoan.carlosama@gmail.com>
2019-11-16 12:07:41 -0500

    Sync

--- src/app/dashboard/modules/financiero/froTrteSolicitud/newRna/newRna.component.ts
+++ src/app/dashboard/modules/financiero/froTrteSolicitud/newRna/newRna.component.ts
@@ -60,6 +60,14 @@ export class NewRnaComponent implements OnInit {
   
   public formApoderado = false;
   public formNewCiudadano: any = false;
+
+  public datosFacturaArchivo = {
+    'numeroFolios': null,
+    'numeroArchivador': null,
+    'bandeja': null,
+    'numeroCaja': null,
+    'idFactura': null,
+  };
   
   constructor(
     private _TramiteSolicitudService: FroTrteSolicitudService,
@@ -533,7 +541,9 @@ export class NewRnaComponent implements OnInit {
     this.tramiteSolicitud.documentacionPendiente = this.documentacionPendiente;
     this.tramiteSolicitud.idFactura = this.factura.id;
 
-    this._TramiteSolicitudService.register(this.tramiteSolicitud, token).subscribe(
+    this.datosFacturaArchivo.idFactura = this.factura.id;
+
+    this._TramiteSolicitudService.register({'tramiteSolicitud': this.tramiteSolicitud, 'datosFacturaArchivo': this.datosFacturaArchivo}, token).subscribe(
 			response => {
         if(response.code == 200){
           this.factura = response.data.factura;
