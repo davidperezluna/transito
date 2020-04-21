75888bf513b3e0394102f70b1b3f24286049b907
Edgar Guerrero <edgargn87@gmail.com>
2020-04-21 10:48:19 -0500

    Update environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,18 +1,18 @@
 export const environment = {
   production: true,
-  //Developer build (ng build --prod --aot=false -base-href http://190.14.247.69/dev/dist/assets)
-  /*apiUrl: 'http://190.14.247.69/dev/colossus-sit/web/app.php/',
-  docsUrl: 'http://190.14.247.69/dev/colossus-sit/web/docs/',
-  uploadUrl: 'http://190.14.247.69/dev/colossus-sit/web/uploads/',
-  imgUrl: 'http://190.14.247.69/dev/colossus-sit/web/img',*/
-  //Production build (ng build --prod --aot=false -base-href http://190.14.247.69/prod/dist/assets)
-  apiUrl: 'http://190.14.247.69/prod/colossus-sit/web/app.php/',
-  docsUrl: 'http://190.14.247.69/prod/colossus-sit/web/docs/',
-  uploadUrl: 'http://190.14.247.69/prod/colossus-sit/web/uploads/',
-  imgUrl: 'http://190.14.247.69/prod/colossus-sit/web/img',
+  //Developer build (ng build --prod --aot=false -base-href http://190.14.247.69:443/dev/dist/assets)
+  /*apiUrl: 'http://190.14.247.69:443/dev/colossus-sit/web/app.php/',
+  docsUrl: 'http://190.14.247.69:443/dev/colossus-sit/web/docs/',
+  uploadUrl: 'http://190.14.247.69:443/dev/colossus-sit/web/uploads/',
+  imgUrl: 'http://190.14.247.69:443/dev/colossus-sit/web/img',*/
+  //Production build (ng build --prod --aot=false -base-href http://190.14.247.69:443/prod/dist/assets)
+  apiUrl: 'http://190.14.247.69:443/prod/colossus-sit/web/app.php/',
+  docsUrl: 'http://190.14.247.69:443/prod/colossus-sit/web/docs/',
+  uploadUrl: 'http://190.14.247.69:443/prod/colossus-sit/web/uploads/',
+  imgUrl: 'http://190.14.247.69:443/prod/colossus-sit/web/img',
   //Local build (ng build --prod --aot=false -base-href https://subdetra.nariño.gov.co/dist/assets)
   /*apiUrl : 'https://subdetra.nariño.gov.co/colossus-sit/web/',
   docsUrl : 'https://subdetra.nariño.gov.co/colossus-sit/web/docs/', 
-  uploadUrl : 'https://subdetra.nariño.gov.co/colossus-sit/web/uploads/', 
+  uploadUrl : 'https://subdetra.nariño.gov.co/colossus-sit/web/uploads/',
   imgUrl : 'https://subdetra.nariño.gov.co/colossus-sit/web/img',*/
 };
