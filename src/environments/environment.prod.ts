8ff3fa1d4b8791561bd687464349c08bc2b1a93b
davidPerez <soporte@jhwebpasto.com>
2021-06-29 19:54:29 -0500

    Sync

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,31 +1,15 @@
 export const environment = {
   production: true,
-  //Developer build (ng build --prod --aot=false -base-href http://190.14.247.69:443/dev/dist/assets)
-  /*apiUrl: 'http://190.14.247.69:443/dev/colossus-sit/web/app.php/',
-  docsUrl: 'http://190.14.247.69:443/dev/colossus-sit/web/docs/',
-  uploadUrl: 'http://190.14.247.69:443/dev/colossus-sit/web/uploads/',
-  imgUrl: 'http://190.14.247.69:443/dev/colossus-sit/web/img',*/
-
-  //Server Production build (ng build --prod --aot=false -base-href http://190.14.247.69:443/prod/dist/assets)
-  // apiUrl: 'http://190.14.247.69:443/prod/colossus-sit/web/app.php/',
-  // docsUrl: 'http://190.14.247.69:443/prod/colossus-sit/web/docs/',
-  // uploadUrl: 'http://190.14.247.69:443/prod/colossus-sit/web/uploads/',
-  // imgUrl: 'http://190.14.247.69:443/prod/colossus-sit/web/img',
-
-  //Server Test build (ng build --prod --aot=false -base-href https://occipruebas.narino.gov.co/dist/assets)
+  //TEST build (ng build --prod --aot=false -base-href https://occipruebas.narino.gov.co//dist/assets)
   apiUrl: 'https://occipruebas.narino.gov.co/colossus-sit/web/app.php/',
   docsUrl: 'https://occipruebas.narino.gov.co/colossus-sit/web/docs/',
   uploadUrl: 'https://occipruebas.narino.gov.co/colossus-sit/web/uploads/',
   imgUrl: 'https://occipruebas.narino.gov.co/colossus-sit/web/img'
 
+  //PROD build (ng build --prod --aot=false -base-href http://sttdn.narino.gov.co/prod/dist/assets)
   /*apiUrl: 'http://sttdn.narino.gov.co/prod/colossus-sit/web/app.php/',
   docsUrl: 'http://sttdn.narino.gov.co/prod/colossus-sit/web/docs/',
   uploadUrl: 'http://sttdn.narino.gov.co/prod/colossus-sit/web/uploads/',
   imgUrl: 'http://sttdn.narino.gov.co/prod/colossus-sit/web/img',*/
 
-  //Local build (ng build --prod --aot=false -base-href http://sttdn.narino.gov.co/prod/dist/assets)
-  /*apiUrl : 'https://subdetra.nariño.gov.co/colossus-sit/web/',
-  docsUrl : 'https://subdetra.nariño.gov.co/colossus-sit/web/docs/', 
-  uploadUrl : 'https://subdetra.nariño.gov.co/colossus-sit/web/uploads/',
-  imgUrl : 'https://subdetra.nariño.gov.co/colossus-sit/web/img',*/
 };
